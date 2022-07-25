3e190cb391af632056d67921b8d3f78005555d07
meta <troctsch.cpp@gmail.com>
2022-07-25 12:20:57 +0200

    chapter_1_concepts
    - first concept, gotten rid off SFINAE for return type

--- src/chapter_1_concepts/tests.cpp
+++ src/chapter_1_concepts/tests.cpp
@@ -4,16 +4,11 @@
 template <typename T, typename... TS>
 constexpr inline bool are_same_v = std::conjunction_v<std::is_same<T, TS>...>;
 
-template <typename T, typename...> struct first_arg { using type = T; };
-template <typename... TS> using first_arg_t = typename first_arg<TS...>::type;
-
 template <typename... Args>
-std::enable_if_t<are_same_v<Args...>, first_arg_t<Args...>>
-Add(const Args &...args) {
-  return (args + ...);
-}
+requires are_same_v<Args...>
+auto Add(Args &&...args) { return (args + ...); }
 
-TEST_CASE("No compilation with SFINAE") {
+TEST_CASE("No compilation with a concept") {
   const int x = Add(2, 3, 4, 5);
   const int y = Add(2, 3);
   // won't compile
