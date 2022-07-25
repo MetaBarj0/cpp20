a0c45fb2190d9f5399840242085502811a73733a
meta <troctsch.cpp@gmail.com>
2022-07-25 12:39:16 +0200

    chapter_1_concepts
    - don't loose the steps from sfinae to concepts

--- docs/reading-the-book.md
+++ docs/reading-the-book.md
@@ -7,6 +7,7 @@ Gonna study Concepts, Coroutines, Ranges and more.
 ## Practical aspects
 
 The book examples run with clang 13, clang 14 should be ok.
+The code is experienced through test cases using catch2
 
 ## Chapter 1: Concepts
 
@@ -16,3 +17,5 @@ They facilitate te writing of template meta programming code.
 They provide better and more explicit error messages for generic template meta programming code misusage.
 
 ### Playing with examples
+
+Got to src/chapter_1_concepts/tests.cpp to get a grasp on the matter.
--- src/chapter_1_concepts/tests.cpp
+++ src/chapter_1_concepts/tests.cpp
@@ -4,13 +4,33 @@
 template <typename T, typename... TS>
 constexpr inline bool are_same_v = std::conjunction_v<std::is_same<T, TS>...>;
 
+template <typename T, typename...> struct first_arg { using type = T; };
+template <typename... TS> using first_arg_t = typename first_arg<TS...>::type;
+
+template <typename... Args>
+std::enable_if_t<are_same_v<Args...>, first_arg_t<Args...>>
+add_sfinae(Args &&...args) {
+  return (... + args);
+}
+
 template <typename... Args>
 requires are_same_v<Args...>
-auto Add(Args &&...args) { return (args + ...); }
+auto add_concepts(Args &&...args) { return (... + args); }
 
-TEST_CASE("No compilation with a concept") {
-  const int x = Add(2, 3, 4, 5);
-  const int y = Add(2, 3);
+TEST_CASE("No compilation with SFINAE") {
+  const int x = add_sfinae(2, 3, 4, 5);
+  const int y = add_sfinae(2, 3);
+  // won't compile
+  // const int z = Add(2, 3.0);
+
+  REQUIRE(x == 14);
+  REQUIRE(y == 5);
+  // REQUIRE(z == 5.0);
+}
+
+TEST_CASE("No compilation with concepts") {
+  const int x = add_concepts(2, 3, 4, 5);
+  const int y = add_concepts(2, 3);
   // won't compile
   // const int z = Add(2, 3.0);
 
